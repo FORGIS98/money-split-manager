0941b690db14a90cbe93a635073b6ed545a8192d
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-08-27 19:45:57 +0200

    Calling other files

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
 use rusqlite::{Connection, Result};
 
+mod database;
+
 fn main() -> Result<()> {
     let conn = Connection::open("money_database.db")?;
 
-    if let Err(e) = create_tables(&conn) {
+    if let Err(e) = database::create_tables(&conn) {
         println!("create_tables() error: {}", e);
     }
 
-    Ok(())
-}
-
-/// Creates two tables, **receipt** to save all the expenses
-/// and **ledger** to know who owes money to whom.
-fn create_tables(conn: &Connection) -> Result<()> {
-    conn.execute(
-        "create table if not exists receipt (
-            id integer primary key,
-            payer text not null,
-            amount integer not null,
-            description text not null
-        )",
-        [],
-    )?;
-
-    conn.execute(
-        "create table if not exists ledger (
-            id integer primary key,
-            borrower text not null,
-            amount integer not null,
-            owner text not null
-        )",
-        [],
-    )?;
+    if let Err(e) = database::add_receipt(&conn, "Paula", &30, "Pizza") {
+        println!("add_receipt() error: {}", e);
+    }
 
     Ok(())
 }
