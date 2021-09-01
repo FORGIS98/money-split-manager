71d099e14c767f9e4b8c128734f9a2907fc739fc
FORGIS98 <jorgesolgonzalez1998@gmail.com>
2021-09-01 19:10:34 +0200

    database selection DONE :D

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rusqlite::Connection;
-use std::{error::Error, fs, io, io::ErrorKind, path::Path};
+use std::{collections::HashMap, error::Error, fs, io, io::ErrorKind, path::Path};
 
 mod database;
 
@@ -14,10 +14,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // First conversation
     println!(
-        "WELCOME to money-split-manager, this not to friendly terminal experience is going to try to walk you through the app :D \n\n
+        "WELCOME to money-split-manager, this not to friendly terminal experience is going to try to walk you through the app :D");
 
-        So, first things first, do you want to create a new manager? Write \"yes\" or \"no\" please:
-        ");
+    println!(""); // Air :D
+
+    println!(
+        "So, first things first, do you want to create a new manager? Write \"yes\" or \"no\" please:");
 
     let new_manager: bool;
     loop {
@@ -61,7 +63,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             let manager_path = format!("./managers/{}.db", all_user_interaction);
 
             // First let's check that the DB doesn't exist already
-            if !std::path::Path::new(&manager_path).exists() {
+            if !Path::new(&manager_path).exists() {
                 conn = Connection::open(&manager_path)?;
                 break;
             } else {
@@ -71,9 +73,54 @@ fn main() -> Result<(), Box<dyn Error>> {
     } else {
         // Show the user a list of available data_bases
         // and let him choose one of them
-    }
+        println!("So this are all the available databases, choose one of them please, (of course, just give me the number): ");
 
-    let conn = Connection::open("money_database.db")?;
+        let mut all_managers = HashMap::new();
+
+        for files in fs::read_dir("./managers/")? {
+            let files = files?;
+            all_managers.insert(
+                (all_managers.len() + 1) as i32,
+                files.path().display().to_string(),
+            );
+        }
+
+        println!("{:#?}", all_managers);
+
+        loop {
+            if all_managers.len() == 0 {
+                println!(
+                    "There is no managers here...they are maybe on vacation...as I wish I was..."
+                );
+                break;
+            }
+
+            let mut all_user_interaction = String::new();
+            io::stdin()
+                .read_line(&mut all_user_interaction)
+                .expect("Failed to read line...to bad :/");
+
+            println!(""); // Air :D
+
+            let all_user_interaction: i32 = match all_user_interaction.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("...I said a number! (Hate this humans)");
+                    continue;
+                }
+            };
+
+            if all_user_interaction < 1 || all_user_interaction > all_managers.len() as i32 {
+                println!("COME ON! You don't even try, I need a number from the list: ");
+                continue;
+            }
+
+            let manager_path = all_managers.get(&all_user_interaction).unwrap();
+            conn = Connection::open(&manager_path)?;
+
+            break;
+        }
+    }
 
     if let Err(e) = database::create_tables(&conn) {
         println!("create_tables() error: {}", e);
